0f633b32f5e2449eea29a3ec13f009324500cea2
CarritoHG <hhernandezc2202@alumno.ipn.mx>
2025-11-22 16:03:41 -0600

    Creacion repo

---
+++ src/main/java/com/zinnith/vectorc5/VectorC5Application.java
@@ -0,0 +1,13 @@
+package com.zinnith.vectorc5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorC5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VectorC5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zinnith/vectorc5/VectorC5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinnith.vectorc5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectorC5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
